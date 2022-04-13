bdcc1c562e6cac60ecbc5cba1d0b16d24f600050
Josiel Souza <josielsouza.dj@gmail.com>
2022-04-12 22:28:34 -0300

    feat: styling home

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-//Testing functionality
+//function constructor
 
-const funcaoSoma = () => {
-    console.log("Josiel")
-}
+ // * EXPRESSÃO NEW 
+ // * CRIAR UM NOVO OBJETO
+ // * THIS KEYMORD
 
-funcaoSoma();
+
+
+//molde da Persona
+ function Persona (name){
+    this.name = name
+    this.andar = function(){
+        return this.name + " Estar andando"
+    }
+ }
+
+
+ //objetos criados da persona
+ const josiel = new Persona("Tadeu")
+ const Maik = new Persona("Maik")
+
+ console.log(josiel.name)
+ console.log(Maik.name)
+
+ console.log(josiel.andar())
+ console.log(Maik.andar())
+    
+
+
+
+
+
