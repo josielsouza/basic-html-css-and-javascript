c0681d131b75f14c9e55f8fb440d7cdae0c7dd82
Josiel Souza <josielsouza.dj@gmail.com>
2022-04-13 22:22:00 -0300

    feat: styling home

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-//function constructor
+//for
+//aqui imprime de 0 a 9
+for (let i = 0; i < 10; i++) {
+   console.log(i);
+}
 
- // * EXPRESSÃO NEW 
- // * CRIAR UM NOVO OBJETO
- // * THIS KEYMORD
+//aqui começa de de 1 a 9
+//para ir até o 10 basta colocar e <= 10;
+for (let e = 1; e < 10; e++) {
+   console.log(e);
+}
 
+//aqui é de 100 a zero decrecente
+for (let a = 100; a > 0; a--) {
+  console.log(a);
+}
 
-
-//molde da Persona
- function Persona (name){
-    this.name = name
-    this.andar = function(){
-        return this.name + " Estar andando"
-    }
- }
-
-
- //objetos criados da persona
- const josiel = new Persona("Tadeu")
- const Maik = new Persona("Maik")
-
- console.log(josiel.name)
- console.log(Maik.name)
-
- console.log(josiel.andar())
- console.log(Maik.andar())
-    
-
-
-
-
-
+//aqui para a aplicação quando chegar no número 5
+for (let b = 0; b < 10; b++) {
+  if (b === 5) {
+    break;
+  }
+  console.log(b);
+}
+//aqui continua, mais pula o 5
+for (let c = 0; c < 10; c++) {
+  if (c === 5) {
+    continue;
+  }
+  console.log(c);
+}
